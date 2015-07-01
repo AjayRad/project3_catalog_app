cc12dd8b9c381b4e9b7f4aefd194716c1765c332
aj <allyshub@gmail.com>
2015-07-01 11:14:05 -0700

    added a couple of client side validations

---
+++ vagrant/catalog/catalogapp/static/js/client_validations.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $("#addCatg").click(function () {
+        var catg_name = document.getElementById('inputTitle').value;
+        
+        if (catg_name.length == 0) {
+            alert("Hey there - Category Name cannot be blank. Please name it ! ");
+            return false;
+        } 
+    });
+    $("#addProduct").click(function () {
+        var product_name = document.getElementById('inputTitle').value;
+        
+        if (product_name.length == 0) {
+            alert("Hey there - Product Name cannot be blank. Please name it ! ");
+            return false;
+        } 
+    });
+
+});
+
